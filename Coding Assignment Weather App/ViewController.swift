998b47016fdc5b2ece7581254206289adf62dff1
Sanip Shrestha <sanip_7@live.com>
2019-02-11 12:23:29 +1100

    Creating a new project.

---
+++ Coding Assignment Weather App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coding Assignment Weather App
+//
+//  Created by Sanip Shrestha on 2/11/19.
+//  Copyright © 2019 Learning. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
